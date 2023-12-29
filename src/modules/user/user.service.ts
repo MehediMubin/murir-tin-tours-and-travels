e511e7283fe4036c77cd1da0ca0545e20c4c3896
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2023-12-29 13:57:23 +0600

    get all user, get single user, update user, delete user services implemented

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,11 +1,41 @@
 import { IUser } from "./user.interface";
 import { UserModel } from "./user.model";
 
-const createUser = async(userData: IUser): Promise<IUser> => {
-    const result = await UserModel.create(userData);
-    return result;
+const createUser = async (userData: IUser): Promise<IUser> => {
+   const result = await UserModel.create(userData);
+   return result;
+};
+
+const getAllUsers = async (): Promise<IUser[]> => {
+   const result = await UserModel.find();
+   return result;
+};
+
+const getSingleUser = async (id: string): Promise<IUser | null> => {
+   const result = await UserModel.findById(id);
+   return result;
+};
+
+const updateUser = async (
+   id: string,
+   userData: IUser,
+): Promise<IUser | null> => {
+   const result = await UserModel.findByIdAndUpdate(id, userData, {
+      new: true,
+      runValidators: true,
+   });
+   return result;
+};
+
+const deleteUser = async (id: string): Promise<IUser | null> => {
+   const result = await UserModel.findByIdAndDelete(id);
+   return result;
 };
 
 export const UserService = {
    createUser,
+   getAllUsers,
+   getSingleUser,
+   updateUser,
+   deleteUser,
 };
