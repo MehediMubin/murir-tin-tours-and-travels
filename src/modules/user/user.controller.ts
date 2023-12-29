1c3d59735bca29b90390e0f24e56a277b6a9195d
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2023-12-29 14:04:06 +0600

    get all users, get single user, update user, delete user controllers implemented

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import { UserModel } from "./user.model";
 import { UserService } from "./user.service";
 
 const createUser = async (req: Request, res: Response) => {
@@ -21,6 +20,86 @@ const createUser = async (req: Request, res: Response) => {
    }
 };
 
+const getAllUsers = async (req: Request, res: Response) => {
+   try {
+      const result = await UserService.getAllUsers();
+      res.status(200).json({
+         success: true,
+         message: "All users fetched successfully",
+         data: result,
+      });
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+   } catch (error: any) {
+      console.log(error);
+      res.status(500).json({
+         success: false,
+         message: error.message || "Internal server error",
+      });
+   }
+};
+
+const getSingleUser = async (req: Request, res: Response) => {
+   try {
+      const id = req.params.id;
+      const result = await UserService.getSingleUser(id);
+      res.status(200).json({
+         success: true,
+         message: "Single user fetched successfully",
+         data: result,
+      });
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+   } catch (error: any) {
+      console.log(error);
+      res.status(500).json({
+         success: false,
+         message: error.message || "Internal server error",
+      });
+   }
+};
+
+const updateUser = async (req: Request, res: Response) => {
+   try {
+      const id = req.params.id;
+      const userData = req.body;
+      const result = await UserService.updateUser(id, userData);
+      res.status(200).json({
+         success: true,
+         message: "User updated successfully",
+         data: result,
+      });
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+   } catch (error: any) {
+      console.log(error);
+      res.status(500).json({
+         success: false,
+         message: error.message || "Internal server error",
+      });
+   }
+};
+
+const deleteUser = async (req: Request, res: Response) => {
+   try {
+      const id = req.params.id;
+      const result = await UserService.deleteUser(id);
+      res.status(200).json({
+         success: true,
+         message: "User deleted successfully",
+         data: result,
+      });
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+   } catch (error: any) {
+      console.log(error);
+      res.status(500).json({
+         success: false,
+         message: error.message || "Internal server error",
+      });
+   }
+};
+
 export const UserController = {
    createUser,
+   getAllUsers,
+   getSingleUser,
+   updateUser,
+   deleteUser,
 };
