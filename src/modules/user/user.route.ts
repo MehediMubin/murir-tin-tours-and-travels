3314121e2b7cd5e0ae9c17c627c6a8d30de36a4e
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2023-12-29 14:11:53 +0600

    user routes defined

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -5,4 +5,12 @@ const router = Router();
 
 router.post("/create-user", UserController.createUser);
 
+router.get("/", UserController.getAllUsers);
+
+router.get("/:id", UserController.getSingleUser);
+
+router.patch("/:id", UserController.updateUser);
+
+router.delete("/:id", UserController.deleteUser);
+
 export const userRoutes = router;
